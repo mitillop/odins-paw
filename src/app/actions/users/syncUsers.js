36cbb28e81e75c8e1a897abec3bcb647045bb002
Daniel Solano <solanomezaangel@gmail.com>
2025-04-22 22:22:00 -0700

    Sync users with db when logged in

--- src/app/actions/users/syncUsers.js
+++ src/app/actions/users/syncUsers.js
@@ -1,26 +1,4 @@
-import prisma from "@/libs/db"
-import { currentUser } from "@clerk/nextjs/server"
-
-export async function syncUserWithDB() {
-  const user = await currentUser()
-
-  if (!user) return
-
-  const existing = await prisma.user.findUnique({
-    where: { clerkId: user.id },
-  })
-
-  if (!existing) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user.emailAddresses[0].emailAddress,
-        name: user.firstName,
-      },
-    })
-  }
-}
-import prisma from "@/libs/db"
+import prisma from "../../../libs/db"
 import { currentUser } from "@clerk/nextjs/server"
 
 export async function syncUserWithDB() {
@@ -41,5 +19,8 @@ export async function syncUserWithDB() {
         imageUrl: user.imageUrl,
       },
     })
+    console.log("User created in DB")
+  } else {
+    console.log("User already exists in DB")
   }
 }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,18 +1,25 @@
-import { auth, currentUser } from '@clerk/nextjs/server'
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { syncUserWithDB } from "../actions/users/syncUsers";
 
 export default async function Page() {
+  await syncUserWithDB();
   // Get the userId from auth() -- if null, the user is not signed in
-  const { userId } = await auth()
+  const { userId } = await auth();
 
   // Protect the route by checking if the user is signed in
   if (!userId) {
-    return <div>Sign in to view this page</div>
+    return <div>Sign in to view this page</div>;
   }
 
   // Get the Backend API User object when you need access to the user's information
-  const user = await currentUser()
-  console.log(user)
+  const user = await currentUser();
+  console.log(user);
 
   // Use `user` to render user details or create UI elements
-  return <div>Welcome, {user.firstName} {user.id} {user.emailAddresses[0].emailAddress}!</div>
+  return (
+    <div className="flex items-center justify-center ">
+      Welcome, {user.firstName}!
+      {/* <img src={user.imageUrl} alt="User Image" className="w-12 h-12"></img> */}
+    </div>
+  );
 }
